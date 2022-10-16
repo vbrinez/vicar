bac4913996c7b2c1bb9ffeed736e6335402ae677
Carlo <73349682+Carl0jgr@users.noreply.github.com>
2022-10-15 21:20:58 -0300

    Modificaciones para app blog

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path,include
 from tienda.views import verproductos
-from blog.views import home,ver,mod,add,registrarUsuario,editarUsuario, eliminarUsuario, modificarUsuario, addpublicacion,addcategoria,registrarCategoria,registrarPublicacion, readme, perfil, articulo
+from blog.views import home,ver,mod,add,registrarUsuario,editarUsuario, eliminarUsuario, modificarUsuario, addpublicacion,addcategoria,registrarCategoria,registrarPublicacion, readme, perfil, articulo, login
 
 urlpatterns = [
     path('readme',readme),
@@ -9,14 +9,15 @@ urlpatterns = [
     path('ver/',ver),
     path('mod',mod),
     path('add/',add),
-    path('registrarUsuario/',registrarUsuario),
-    path('editarUsuario/<id>/',editarUsuario),
-    path('modificarUsuario/',modificarUsuario),
-    path('eliminarUsuario/<id>/',eliminarUsuario),
-    path('addpublicacion',addpublicacion),
-    path('addcategoria',addcategoria),
-    path('registrarCategoria/',registrarCategoria),
-    path('registrarPublicacion/',registrarPublicacion),
-    path('perfil/<id>/',perfil),
-    path('articulo/<id>/',articulo),
+    path('registrarUsuario/', registrarUsuario),
+    path('editarUsuario/<id>/', editarUsuario),
+    path('modificarUsuario/', modificarUsuario),
+    path('eliminarUsuario/<id>/', eliminarUsuario),
+    path('addpublicacion', addpublicacion),
+    path('addcategoria', addcategoria),
+    path('registrarCategoria/', registrarCategoria),
+    path('registrarPublicacion/', registrarPublicacion),
+    path('perfil/<id>/', perfil),
+    path('articulo/<id>/', articulo),
+    
 ]
